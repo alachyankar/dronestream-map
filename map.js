425394faf52f69696a85718b12cb6d009007fbac
alachyankar <aniketlachyankar@me.com>
2014-10-28 22:34:35 -0400

    changed the representation to circles as opposed to heatmap, seems to work better

--- map.js
+++ map.js
@@ -3,20 +3,28 @@
 var places;
 var url = 'http://www.api.dronestre.am/data?callback=?';
 
-function parseStrikes(data) { 
-    var result = []
-    $(data).each(function(id,element){
-        var lat = element.lat;
-        var long = element.lon;
-        var weightDeath = element.deaths_max;
-        var weightedLocation = {
-            location : new google.maps.LatLng(lat, long),
-            weight = weightDeath
-        };
-        result.push(weightedLocation);
-    })
-    return result;
-}
+// function parseStrikes(data) { 
+//     var result = []
+//     $(data).each(function(id,element){
+//         var lat = element.lat;
+//         var long = element.lon;
+//         var location = new google.maps.LatLng(lat, long);
+//         var deaths = element.deaths_maxl
+//         var popOptions = {
+//             strokeColor : '#FF0000',
+//             strokeOpacity: 0.8,
+//             strokeWeight: 2,
+//             fillColor: '#FF0000',
+//             fillOpacity: 0.35,
+//             map: map,
+//             center: location,
+//             radius: deaths * 10000000000
+//         }
+
+//         cityCircle = new google.maps.Circle(popOptions);
+        
+//     })
+// }
 
     
  function ajaxGet() {
@@ -31,30 +39,62 @@ function parseStrikes(data) {
         }).fail(function(e) {
             console.log(e.message);
         }).always(function(json) {
+
+var citymap = {};
+citymap['chicago'] = {
+  center: new google.maps.LatLng(41.878113, -87.629798),
+  population: 2714856
+};
+citymap['newyork'] = {
+  center: new google.maps.LatLng(40.714352, -74.005973),
+  population: 8405837
+};
+citymap['losangeles'] = {
+  center: new google.maps.LatLng(34.052234, -118.243684),
+  population: 3857799
+};
+citymap['vancouver'] = {
+  center: new google.maps.LatLng(49.25, -123.1),
+  population: 603502
+};
+
          function initialize() {
 
             var mapOptions = {
             zoom: 5,
             center: new google.maps.LatLng(25.774546, 65.433523),
-            mapTypeId: google.maps.MapTypeId.SATELLITE
+            mapTypeId: google.maps.MapTypeId.SATTELITE
+            };   
+
+            map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+
+             // Construct the circle for each value in citymap.
+  // Note: We scale the area of the circle based on the population.
+
+
+            $.each(json.strike, function(index,element){
+                var lat = element.lat;
+                var long = element.lon;
+                var location = new google.maps.LatLng(lat, long);
+                var deaths = element.deaths_max;
+                console.log(deaths);
+                var popOptions = {
+                    strokeColor : '#FF0000',
+                    strokeOpacity: 0.8,
+                    strokeWeight: 2,
+                    fillColor: '#FF0000',
+                    fillOpacity: 0.35,
+                    map: map,
+                    center: location,
+                    radius: deaths * 1000
+                };
+
+                var cityCircle = new google.maps.Circle(popOptions);
+            })
         };
-
-        map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-
-        var pointArray = new google.maps.MVCArray(parseStrikes(json.strike));
-
-        
-        heatmap = new google.maps.visualization.HeatmapLayer({
-            data: pointArray
-        });
-
-        heatmap.setMap(map);
-        }
-        google.maps.event.addDomListener(window, 'load', initialize);
-        $.each(parseStrikes(json.strike),function(index,data) {
-            pointArray
-     })}),
+         google.maps.event.addDomListener(window, 'load', initialize);
+        })
  }
 
 ajaxGet()
