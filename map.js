9de3c50ec99833964f2f4dfef30868ce6df1486b
alachyankar <aniketlachyankar@me.com>
2014-10-29 13:00:57 -0400

    cleaned code

--- map.js
+++ map.js
@@ -3,28 +3,6 @@
 var places;
 var url = 'http://www.api.dronestre.am/data?callback=?';
 
-// function parseStrikes(data) { 
-//     var result = []
-//     $(data).each(function(id,element){
-//         var lat = element.lat;
-//         var long = element.lon;
-//         var location = new google.maps.LatLng(lat, long);
-//         var deaths = element.deaths_maxl
-//         var popOptions = {
-//             strokeColor : '#FF0000',
-//             strokeOpacity: 0.8,
-//             strokeWeight: 2,
-//             fillColor: '#FF0000',
-//             fillOpacity: 0.35,
-//             map: map,
-//             center: location,
-//             radius: deaths * 10000000000
-//         }
-
-//         cityCircle = new google.maps.Circle(popOptions);
-        
-//     })
-// }
 
     
  function ajaxGet() {
@@ -40,24 +18,6 @@ var url = 'http://www.api.dronestre.am/data?callback=?';
             console.log(e.message);
         }).always(function(json) {
 
-var citymap = {};
-citymap['chicago'] = {
-  center: new google.maps.LatLng(41.878113, -87.629798),
-  population: 2714856
-};
-citymap['newyork'] = {
-  center: new google.maps.LatLng(40.714352, -74.005973),
-  population: 8405837
-};
-citymap['losangeles'] = {
-  center: new google.maps.LatLng(34.052234, -118.243684),
-  population: 3857799
-};
-citymap['vancouver'] = {
-  center: new google.maps.LatLng(49.25, -123.1),
-  population: 603502
-};
-
          function initialize() {
 
             var mapOptions = {
@@ -70,7 +30,7 @@ citymap['vancouver'] = {
                 mapOptions);
 
              // Construct the circle for each value in citymap.
-  // Note: We scale the area of the circle based on the population.
+             // Note: We scale the area of the circle based on the population.
 
 
             $.each(json.strike, function(index,element){
